c60fcb4c4b6d14095866ddc9321d8ebe603f7450
gihanswift <165775719+gihanswift@users.noreply.github.com>
2024-04-03 15:03:23 +0530

    Add files via upload

---
+++ DSA-Array.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var strings = ["a", "b", "c", "d"]
+//4 * 4 = 16 bytes of storage
+
+strings[2]
+
+//push
+strings.append("e") // O(1)
+
+
+//pop
+strings.popLast()
+print(strings)
+strings.popLast() // O(1) -> We are not looping through items
+
+
+//Insert
+strings.insert("x", at: 0) //O(n) we have to line the indexes.
+print(strings)
+
+//Insert into middle of the array
+strings.insert("Aliece", at: strings.count / 2) // O(n)
+print(strings)
+
+
+
+
+
+
+
